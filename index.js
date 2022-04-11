8e9b65c3c2d18988043a17edabd35dc6a3e88323
Alex Munoz <alex.munoz905@gmail.com>
2022-04-11 22:18:10 +0100

    Added CLI arguments and CSV

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
+// Modules
 const MongoClient = require('mongodb').MongoClient;
-const authInfo = require("./auth.json");
 const ping = require('ping');
-const hosts = [
-    '1.1.1.1',
-    '8.8.8.8',
-    '8.8.4.4'
-]
+const csv = require('csv-array');
+// Variables from auth.json
+const authInfo = require("./auth.json");
 const url = authInfo.mongoURL;
 const dbName= "UptimeKeeper";
 const collectionName = "deadServers";
 const accountSid = authInfo.accountSid;
 const authToken = authInfo.authToken;
+// Declare the client information for twilio
 const client = require('twilio')(accountSid, authToken);
+// Set date & time
 var dateTime = new Date().toString();
-var minutes = 1, the_interval = minutes * 60 * 1000;
-
-setInterval(function() {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db(dbName);
-        hosts.forEach(function (host) {
-            ping.promise.probe(host, {
-                timeout: 2,
-            }).then(function (res) {
-                console.log(res.alive ? "host: " + host + " is alive!" : "host: " + host + " is dead!");
-                if (res.alive == false) {
-                    var deadHostObj = {date: dateTime, host: host}
-                    dbo.collection(collectionName).insertOne(deadHostObj, function(err, res) {
-                        if (err) throw err;
-                        client.messages.create({
-                            body: 'Host: ' + host + ' went down at ' + dateTime + '!',
-                            from: authInfo.fromNumber,
-                            to: authInfo.cellNumber
-                        }).then(message => console.log(message.sid));
-                    });
-                }
+// Take command arguments from the CLI, and assign them to variables
+var commandLineArguments = process.argv;
+var csvFile = commandLineArguments[2];
+var minutesToUse = commandLineArguments[3];
+// Sets how many minutes to wait based on the CLI
+var minutes = minutesToUse, the_interval = minutes * 60 * 1000;
+// The part that like, uhh, does everything. I think...
+csv.parseCSV(csvFile, function(hosts){
+    setInterval(function() {
+        // Connects to MongoDB, completly optional
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db(dbName);
+            // pings each host, for how many there are
+            hosts.forEach(function (host) {
+                ping.promise.probe(host, {
+                    timeout: 2,
+                }).then(function (res) {
+                    console.log(res.alive ? "host: " + host + " is alive!" : "host: " + host + " is dead!");
+                    if (res.alive == false) {
+                        var deadHostObj = {date: dateTime, host: host}
+                        dbo.collection(collectionName).insertOne(deadHostObj, function(err, res) {
+                            if (err) throw err;
+                            client.messages.create({
+                                body: 'Host: ' + host + ' went down at ' + dateTime + '!',
+                                from: authInfo.fromNumber,
+                                to: authInfo.cellNumber
+                            }).then(message => console.log("Message sent successfully!"));
+                        });
+                    }
+                });
             });
         });
-    });
-}, the_interval);
+    }, the_interval);
+}, false);
